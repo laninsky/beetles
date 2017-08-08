06e0d84d16bffc53c300693d3ed0ba9cee54f2da
Alana Alexander <laninsky@gmail.com>
2017-08-08 12:15:00 -0500

    Update downsampling_fastq.R

--- downsampling_fastq.R
+++ downsampling_fastq.R
@@ -1,6 +1,8 @@
-linenos <- as.matrix(read.table("linestosample"))
+library(data.table)
 
-newmatrix <- matrix("NA,ncol=1,nrow=(dim(linenos)[1]*4))
+linenos <- as.matrix(fread("linestosample",integer64="numeric"))
+
+newmatrix <- matrix("NA",ncol=1,nrow=(dim(linenos)[1]*4))
 newmatrix[(seq(1,dim(newmatrix)[1],4)),1] <- linenos[,1]
 #check this coding is correct
 newmatrix[(seq(2,dim(newmatrix)[1],4)),1] <- linenos[,1]+1
@@ -11,8 +13,8 @@ newmatrix[(seq(4,dim(newmatrix)[1],4)),1] <- linenos[,1]+3
 
 rm(linenos)
 
-file <- as.matrix(read.table("filename"))
+file <- as.matrix(fread("filename"))
 
-output <- file[newmatrix,]
+output <- file[newmatrix[,1],]
 
 write.table(output, "downsampled.fastq",quote=FALSE, col.names=FALSE,row.names=FALSE)
