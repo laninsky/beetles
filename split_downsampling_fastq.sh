414be3e3fc96117b1598fb017451bc24041dd251
Alana Alexander <laninsky@gmail.com>
2017-08-08 18:47:01 -0500

    Update split_downsampling_fastq.sh

--- split_downsampling_fastq.sh
+++ split_downsampling_fastq.sh
@@ -1,12 +1,16 @@
 fastqlineno=`wc -l $filename | awk '{print $1}'`
+echo "Your file has the following number of lines" $fastqlineno
 
 split -l $(( fastqlineno / 2)) $filename
 
 for i in xa*;
 do fastqlineno=`wc -l $i | awk '{print $1}'`
+echo "Your file, " $i " has the following number of lines" $fastqlineno
 if (( $fastqlineno > 2147483647 ));
-then echo "You will need to split your fastq file further in order to proceed as it still has too many lines to be read in by R"; 
-else sampleevery=`echo "scale=0 ; 1 / $perc_to_sample" | bc`;
+then
+echo "You will need to split your fastq file further in order to proceed as it still has too many lines to be read in by R"; 
+else
+sampleevery=`echo "scale=0 ; 1 / $perc_to_sample" | bc`;
 seq 1 $(( sampleevery * 4 )) $fastqlineno > linestosample;
 echo $i > filename;
 Rscript downsampling_fastq.R;
