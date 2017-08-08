20e66cab3d39625f3b3f81caf259bba18d51aed9
Alana Alexander <laninsky@gmail.com>
2017-08-08 10:03:46 -0500

    Update downsampling_fastq.sh

--- downsampling_fastq.sh
+++ downsampling_fastq.sh
@@ -1,6 +1,5 @@
 fastqlineno=`wc -l $filename | awk '{print $1}'`
-sampleevery=`$(( 1 / perc_to_sample ))`
-sampleevery=`$(( sampleevery * 4 ))`
-seq 1 $sampleevery $fastqlineno > linestosample
+sampleevery=`echo "scale=0 ; 1 / $perc_to_sample" | bc`
+seq 1 $(( sampleevery * 4 )) $fastqlineno > linestosample
 echo $filename > filename
 Rscript downsampling_fastq.R
