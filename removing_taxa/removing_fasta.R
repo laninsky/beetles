c5e9bf8d51f0f5694b942d4e3970d037ddf1acd5
Alana Alexander <laninsky@gmail.com>
2019-10-08 12:46:57 +1300

    Create removing_fasta.R

---
+++ removing_taxa/removing_fasta.R
@@ -0,0 +1,2 @@
+# Removing a specific taxon from multiple fasta files in a folder
+# (i.e. to look at the affect of that taxon, but not modifying data like if prevous matrix assembly steps were rerun)
